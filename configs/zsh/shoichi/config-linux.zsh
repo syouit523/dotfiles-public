ed1d3904e00ee96a06c14385fd0e5cf0eee84e24
shoichi <tshoichi523@gmail.com>
2024-10-31 01:33:33 +0900

    add diff-highlight path

--- configs/zsh/shoichi/config-linux.zsh
+++ configs/zsh/shoichi/config-linux.zsh
@@ -1,5 +1,7 @@
 #linuxbrew
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+#export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 
 #diff-highlight
- export PATH=" PATH=$PATH:$(brew --prefix git)/share/git-core/contrib/diff-highlight:$PATH"   
+ export PATH=" PATH=$PATH:$(brew --prefix git)/share/git-core/contrib/diff-highlight:$PATH"
+ 

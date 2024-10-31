8f5a8d6eefd64d8f8c3ba1536d6043bbd8f76c9c
shoichi <tshoichi523@gmail.com>
2024-10-31 22:27:26 +0900

    fix linux zsh

--- configs/zsh/shoichi/config-linux.zsh
+++ configs/zsh/shoichi/config-linux.zsh
@@ -1,6 +1,7 @@
 #linuxbrew
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-#export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+
+export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
 
 #diff-highlight
  export PATH=" PATH=$PATH:$(brew --prefix git)/share/git-core/contrib/diff-highlight:$PATH"

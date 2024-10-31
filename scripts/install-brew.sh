1ac4347789ddb6ee582e3b8321ef1fdc6bb0cd10
shoichi <tshoichi523@gmail.com>
2024-11-01 00:48:07 +0900

    fix brew shell

--- scripts/install-brew.sh
+++ scripts/install-brew.sh
@@ -3,8 +3,8 @@
 #/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 if command -v brew >/dev/null 2>&1; then
-    sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
-else
     echo "brew is already installed."
     echo "skip installing brew"
+else
+    sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
 fi

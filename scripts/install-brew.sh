5a85751ae61edc7cb544c33b26f997056adb2c02
Shoichi Taguchi <tshoichi523@gmail.com>
2024-10-31 16:55:12 +0900

    Update install-brew.sh

--- scripts/install-brew.sh
+++ scripts/install-brew.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+if command -v brew >/dev/null 2>&1; then
+    sudo curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+else
+    echo "brew is already installed."
+    echo "skip installing brew"
+fi

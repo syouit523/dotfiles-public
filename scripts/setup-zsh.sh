50e1b0f4cbed080d4182c3f2ffa94fe7f8c66079
shoichi <tshoichi523@gmail.com>
2024-11-05 21:11:59 +0900

    fix setup-zsh

--- scripts/setup-zsh.sh
+++ scripts/setup-zsh.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
 if command -v zsh >/dev/null 2>&1; then
-    # set to the default shell to zsh
-    chsh -s $(which zsh)
-    zdh
-    # install oh-my-zsh
-    rm -rf ${HOME}/.oh-my-zsh
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    zsh -c 'source ~/.zshrc'
+    # # set to the default shell to zsh
+    # chsh -s $(which zsh)
+    # zdh
+    # # install oh-my-zsh
+    # sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    # source ~/.zshrc
+    # # theme
+    # git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+    # # install plugins of oh-my-zsh
+    # ## zsh-autosuggestions
+    # git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+    # ## zsh-syntax-highlighting
+    # git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+    # install oh-my-zsh without running zsh immediately
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# source .zshrc if zsh is the current shell
+    if [ -n "$ZSH_VERSION" ]; then
+    source ~/.zshrc
+    fi
+
     # theme
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
@@ -15,7 +31,8 @@ if command -v zsh >/dev/null 2>&1; then
     ## zsh-autosuggestions
     git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     ## zsh-syntax-highlighting
-    git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
 
 else
     echo "zsh is not installed."

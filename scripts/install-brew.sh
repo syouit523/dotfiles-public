046f2fb3ac461e97c80fc71671beed96073f5ad1
Shoichi Taguchi <tshoichi523@gmail.com>
2024-11-07 15:07:19 +0900

    Update install-brew.sh

--- scripts/install-brew.sh
+++ scripts/install-brew.sh
@@ -13,6 +13,7 @@ else
         # for Apple silicon
 	    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
 	    eval "$(/opt/homebrew/bin/brew shellenv)"
+     	    source $HOME/.zprofile
         fi
     fi
 fi

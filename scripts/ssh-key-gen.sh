459b6bd3ee5a52b3d3746646e44fd9f73a2aa2a3
shoichi <tshoichi523@gmail.com>
2024-11-09 18:18:39 +0900

    fix to use gh login command insted of ssh-key-gen

--- scripts/ssh-key-gen.sh
+++ scripts/ssh-key-gen.sh
@@ -1,33 +1,29 @@
 #!/bin/bash
 
 connection_test_github () {
-    echo "DO YOU WANT TO CONNECTION TEST TO GITHUB?: y/n"
+    echo "DO YOU WANT TO CONNECTION TEST TO GITHUB? (Y/n)"
     read flag
-    if [ $flag = "y" ]; then
+    if [ "$flag" = "Y" ]; then
         ssh -T git@github.com
     fi
 }
-
 
 # SSHキーが既に存在するか確認
 KEY_PATH_ED25519="$HOME/.ssh/id_ed25519"
 if [ -f "$KEY_PATH_ED25519" ]; then
     echo "SSHキーは既に存在します。"
-    connection_test_github
+    connection_test_github || exit 0
 else
     echo "SSHキーが見つかりません。新しいキーを作成します。"
-    ## SET E-MAIL
-    echo "INPUT YOUR e-mail ADDRESS:"
-    read mail
-    ssh-keygen -t ed25519 -C ${mail}
-    eval "${ssh-agant -s}"
-    ssh-add $HOME/.ssh/id_ed25519
-    echo "新しいSSHキーが作成されました: $KEY_PATH"
-    ## PRINT THE KEY
-    echo "Please add the pub-key on Github!"
-    bat $HOME/.ssh/id_ed25519.pub
+    
+    # GitHub CLIで認証およびSSHキーの自動作成・追加
+    echo "GitHubアカウントにログインし、SSHキーを登録します。"
+    gh auth login -p ssh
+
+    # 接続テスト
     connection_test_github
-    ## change to ssh connection on this repository
-    git remote set-url origin git@github.com:syouit523/dotfiles-public.git
+    
+    # リモートURLをSSH接続に変更
+    git remote set-url origin git@github.com:syouit523/dotfiles-public.git || exit 0
 fi
 

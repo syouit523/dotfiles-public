1bd069839204d4b93d7b56bdd4a11c9dd92b5e36
shoichi <tshoichi523@gmail.com>
2024-11-11 23:47:08 +0900

    fix deploy

--- scripts/deploy-configs.sh
+++ scripts/deploy-configs.sh
@@ -12,10 +12,35 @@ if [[ -z "$MODE" || -z "$ROOT_DIR" ]]; then
   exit 1
 fi
 
-deploy_git () {
+mode_file() {
+  local source_file="$1"
+  local target_file="$2"
+  mkdir -p "$(dirname "$target_file")"
   case "$MODE" in
     link)
-    ln -sf "$1"/gitignore_global ~/.gitignore_global
+      ln -sf "$source_file" "$target_file"
+      ;;
+    copy)
+      cp --remove-destination "$source_file" "$target_file"
+      ;;
+    delete)
+      unlink "$target_file"
+      ;;
+  esac
+}
+
+
+mode_directory() {
+    local source_dir="$1"
+    local target_dir="$2"
+    find "$source_dir" -type f | while read -r file; do
+        local relative_path="${file#$source_dir/}"
+        mode_file "$file" "$target_dir/$relative_path"
+    done
+}
+
+deploy_git () {
+    mode_file "$1/gitignore_global" "${HOME}/.gitignore_global"
     cp --remove-destination "$1"/gitconfig ~/.gitconfig # copy it since modify user config after
     ## SET USER CONFIG INTO COMPANY DIR
     echo "DO YOU WANT TO SET COMPANY USER INFO?: y/n"
@@ -44,122 +69,32 @@ EOS
   path = ${COMPANY_CONFIG}
 EOS
     fi
-      ;;
-    copy)
-      cp --remove-destination "$1"/gitignore_global ${HOME}/.gitignore_global
-      # not linked .gitconfig
-      ;;
-    delete)
-      rm -f "~/.gitconfig"
-      rm -f "~/.gitignore_global"
-      ;;
-  esac
 }
 
 deploy_vim () {
-  case "$MODE" in
-    link)
-      ln -sf "$1"/gvimrc ~/.gvimrc
-      ln -sf "$1"/vimrc ~/.vimrc
-      ;;
-    copy)
-      cp --remove-destination "$1"/gvimrc ~/.gvimrc
-      cp --remove-destination "$1"/vimrc ~/.vimrc
-      ;;
-    delete)
-      rm -f ~/.gvimrc
-      rm -f ~/.vimrc
-      ;;
-  esac
+  mode_file "$1/gvimrc" "${HOME}/.gvimrc"
+  mode_file "$1/vimrc" "${HOME}/.vimrc"
 }
 
 deploy_zsh () {
-  case "$MODE" in
-    link)
-      ln -sf "$1"/zshrc ~/.zshrc
-      ln -sf "$1"/p10k.zsh ~/.p10k.zsh
+      mode_file "$1/zshrc" "${HOME}/.zshrc"
+      mode_file "$1/p10k.zsh" "${HOME}/.p10k.zsh"
       mkdir -p ~/.config/zsh/shoichi/
-      sudo -n ln -sf "$1"/shoichi/* ~/.config/zsh/shoichi/
-    ;;
-    copy)
-      cp --remove-destination "$1"/zshrc ~/.zshrc
-      cp --remove-destination "$1"/p10k.zsh ~/.p10k.zsh
-      cp -r --remove-destination "$1"/shoichi/ ~/.config/zsh/shoichi/
-      ;;
-    delete)
-      rm -f ~/.zshrc
-      rm -f ~/.p10k.zsh
-      rm -rf ~/.config/zsh/
-      ;;
-  esac
+      mode_directory "$1/shoichi" "${HOME}/.config/zsh/shoichi"
 }
 
 deploy_fish () {
-  mkdir -p ~/.config/fish
-  case "$MODE" in
-    link)
-      sudo -n ln -sf "$1"/config/fish/* ~/.config/fish/
-      ;;
-    copy)
-      cp -r --remove-destination "$1"/config/fish/ ~/.config/fish/
-      ;;
-    delete)
-      rm -rf ~/.config/fish/
-      ;;
-  esac
+  mode_directory "$1/config/fish" "${HOME}/.config/fish"
 }
 
 deploy_vscode () {
-  case "$MODE" in
-    link)
-      sudo -n mkdir ~/Library/Application\ Support/Code/User/
-      sudo -n ln -sf "$1"/settings.json ~/Library/Application\ Support/Code/User/
-      ;;
-    copy)
-      sudo -n cp --remove-destination "$1"/settings.json ~/Library/Application\ Support/Code/User/settings.json
-      ;;
-    delete)
-      sudo rm -rf ~/Library/Application Support/Code/User/settings.json
-      ;;
-  esac
+      local vscode_dir="${HOME}/Library/Application Support/Code/User"
+      sudo -n mkdir "$vscode_dir"
+      sudo -n mode_file "{$1}/settings.json" "$vscode_dir/settings.json"
 }
 
 deploy_nvim () {
-  target_dir="${HOME}/.config/nvim"
-  source_dir=""$1""
-  case "$MODE" in
-    link)
-      mkdir -p ~/.config/nvim/
-
-      # target_dir に source_dir 内のすべてのファイルのシンボリックリンクを作成
-      find "$source_dir" -type f | while read -r file; do
-        # リンク先のパスを生成
-        link_path="$target_dir/${file#$source_dir/}"
-        # 必要なディレクトリを作成
-        mkdir -p "$(dirname "$link_path")"
-        # シンボリックリンクを作成
-        [ ! -e "$link_path" ] || rm "$link_path"  # 既存のリンクを削除
-        ln -sf "$file" "$link_path"
-      done
-      ;;
-    copy)
-      find "$source_dir" -type f | while read -r file; do
-        # リンク先のパスを生成
-        link_path="$target_dir/${file#$source_dir/}"
-        # 必要なディレクトリを作成
-        mkdir -p "$(dirname "$link_path")"
-        # シンボリックリンクを作成
-        [ ! -e "$link_path" ] || rm "$link_path"  # 既存のリンクを削除
-        cp --remove-destination "$file" "$link_path"
-      done
-      ;;
-    delete)
-      # source_dir 内のすべてのファイルを削除
-      find "$target_dir" -type f | while read -r file; do
-        rm -f "$file"
-      done
-      ;;
-  esac
+  mode_directory "$1" "${HOME}/.config/nvim"
 }
 
 for DIR_FULLPATH in $(find "$CONFIGS" -not -path '*/\.*' -mindepth 1 -maxdepth 1 -type d); do
@@ -172,5 +107,6 @@ for DIR_FULLPATH in $(find "$CONFIGS" -not -path '*/\.*' -mindepth 1 -maxdepth 1
     "fish" ) deploy_fish $DIR_FULLPATH;;
     "vscode" ) deploy_vscode $DIR_FULLPATH;;
     "nvim" ) deploy_nvim $DIR_FULLPATH;;
+    * ) echo "No deployment function for ${DIR_NAME}";;
   esac
 done

ed0855372c191209e55285294a118359af633aa6
Shoichi Taguchi <tshoichi523@gmail.com>
2025-04-01 01:07:07 +0900

    remove linuxbrew path

--- configs/zsh/shoichi/config-linux.zsh
+++ configs/zsh/shoichi/config-linux.zsh
@@ -1,8 +1,3 @@
-#linuxbrew
-eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-
-export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
-
 #diff-highlight
 export PATH=" PATH=$PATH:$(brew --prefix git)/share/git-core/contrib/diff-highlight:$PATH"
  
